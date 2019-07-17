ba159d61edb24d0301362df9ffd46353f8fabbd3
admin <admin@192.168.0.11>
2019-07-17 22:01:43 +0530

    success

--- src/main/java/com/st/SpringBootWithMysqlApplication.java
+++ src/main/java/com/st/SpringBootWithMysqlApplication.java
@@ -2,8 +2,10 @@ package com.st;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootWithMysqlApplication {
 
 	public static void main(String[] args) {
